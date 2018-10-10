3345c08173581a306f67edc5b8982f51e16be7b5
jcalef <johnmcalef@gmail.com>
2018-10-10 14:05:34 -0700

    Added configurations and Gemfiles for smoke tests (#1)
    Show me potato salad!!!!

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'byebug'
+gem "json"
+gem "selenium-webdriver"
+gem "webdriver-firefox"
+gem "geckodriver-helper"
+gem "rspec"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,8 @@ require 'katalon_helpers'
 include RSpec::Expectations
 
 def configure_driver
-  @driver = Selenium::WebDriver.for :firefox #, options: options
+  options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
+  @driver = Selenium::WebDriver.for :firefox , options: options
   @accept_next_alert = true
   @driver.manage.timeouts.implicit_wait = 30
   @base_url = "https://courseware-staging.strongmind.com/"
