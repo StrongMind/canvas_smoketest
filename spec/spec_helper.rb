d5c4799c15359f2838ae6f36c65fe46c8b7c06a8
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-10-19 14:12:16 -0700

    tests for color picker

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,11 @@ require "json"
 require "selenium-webdriver"
 require "rspec"
 require 'examples/student_examples'
-require 'katalon_helpers'
+# require 'katalon_helpers'
 include RSpec::Expectations
 
 def configure_driver
-  @driver = Selenium::WebDriver.for :firefox #, options: options
+  @driver = Selenium::WebDriver.for :firefox
   @accept_next_alert = true
   @driver.manage.timeouts.implicit_wait = 30
   @base_url = "https://courseware-staging.strongmind.com/"
--- spec/students/course_cards_spec.rb
+++ spec/students/course_cards_spec.rb
@@ -1,42 +1,70 @@
 require 'spec_helper'
 
-describe "A student course card" do
+describe "A student viewing a course card" do
   before(:all) { configure_driver }
   after(:all)  { @driver.quit }
 
   include_examples "student login"
 
   it 'I can see a title' do
-    expect { @driver.find_element(class: 'ic-DashboardCard__header-title') }.to_not raise_error
+    expect {
+      @driver.find_element(class: 'ic-DashboardCard__header-title')
+    }.to_not raise_error
   end
 
   it 'I can see a subtitle' do
-    expect { @driver.find_element(class: 'ic-DashboardCard__header-subtitle') }.to_not raise_error
+    expect {
+      @driver.find_element(class: 'ic-DashboardCard__header-subtitle')
+    }.to_not raise_error
   end
 
   it 'I can see a letter grade' do
-    expect { @driver.find_element(class: 'sm-DashboardCard__course_grade') }.to_not raise_error
+    expect {
+      @driver.find_element(class: 'sm-DashboardCard__course_grade')
+    }.to_not raise_error
   end
 
   it 'I can see a progress bar' do
-    expect { @driver.find_element(class: 'sm-DashboardCard__progress-container') }.to_not raise_error
+    expect {
+      @driver.find_element(class: 'sm-DashboardCard__progress-container')
+    }.to_not raise_error
   end
 
   context 'navigation buttons' do
     it "I can see announcements" do
-      expect { @driver.find_element(class: 'icon-announcement') }.to_not raise_error
+      expect {
+        @driver.find_element(class: 'icon-announcement')
+      }.to_not raise_error
     end
+
     it "I can see assignments" do
-      expect { @driver.find_element(class: 'icon-assignment') }.to_not raise_error
+      expect {
+        @driver.find_element(class: 'icon-assignment')
+      }.to_not raise_error
     end
     it "I can see discussions" do
-      expect { @driver.find_element(class: 'icon-discussion') }.to_not raise_error
+      expect {
+        @driver.find_element(class: 'icon-discussion')
+      }.to_not raise_error
     end
     it "I can see folder" do
-      expect { @driver.find_element(class: 'icon-folder') }.to_not raise_error
+      expect {
+        @driver.find_element(class: 'icon-folder')
+      }.to_not raise_error
     end
   end
 
-  it "I can change the course nickname"
+  it "I can change the course nickname" do
+    @driver.find_element(class: 'ic-DashboardCard__header-button').click
+    nickname_field = @driver.find_element(id: 'NicknameInput')
+
+    name = 'new_course_name' + Time.now.strftime('%s')
+    nickname_field.clear
+    nickname_field.send_keys(name)
+    @driver.find_element(class: 'Button--primary').click
+    @driver.navigate.refresh
+    sleep(1)
+    expect(@driver.find_element(class: 'ic-DashboardCard__header-title span').text).to eq(name)
+  end
   it "I can change the course color"
 end
