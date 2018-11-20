eef0a81bf555ddcb5f6b1d32641755f88442db0a
Tesla Team <tesla@strongmind.com>
2018-11-20 10:25:00 -0700

    configuration to run headless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,10 @@ require "json"
 require "selenium-webdriver"
 require "rspec"
 require 'examples/student_examples'
-# require 'katalon_helpers'
 include RSpec::Expectations
 
 def configure_driver
-  options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
+  options = Selenium::WebDriver::Firefox::Options.new(args: [ENV['SHOW_BROWSER'] ? nil : '-headless'])
   @driver = Selenium::WebDriver.for :firefox, options: options
   @accept_next_alert = true
   @driver.manage.timeouts.implicit_wait = 30
