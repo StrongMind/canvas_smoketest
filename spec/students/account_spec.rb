d06fa2b1bb90fb815c29ca99dbb837ea9e5b65f6
Sarah Albinda <sarah.albinda@strongmind.com>
2018-11-21 10:07:43 -0700

    Account tests and teacher tests incoming

--- spec/students/account_spec.rb
+++ spec/students/account_spec.rb
@@ -21,13 +21,23 @@ describe "account as a student" do
     it 'I can add a new contact method' do
       @driver.get "https://courseware-staging.strongmind.com/profile/settings"
       @driver.find_element(:link, "Email Address").click
+      @driver.find_element(:id, "ui-id-1").click
       @driver.find_element(:id, "communication_channel_email").click
       @driver.find_element(:id, "communication_channel_email").clear
-      @driver.find_element(:id, "communication_channel_email").send_keys "regression1@strongmind.com"
+      @driver.find_element(:id, "communication_channel_email").send_keys "test@test.com"
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Email Address'])[2]/following::button[1]").click
-      @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Re-Send Confirmation'])[2]/following::button[1]").click
-      expect { @driver.find_element(:css, "a.email_channel") }.to_not raise_error
+      @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Confirm Email Address'])[1]/following::span[1]").click
     end
-
   end
+
+  context 'I can access and update my notification settings' do
+
+    it 'I can change my notification settings' do
+      @driver.get "https://courseware-staging.strongmind.com/profile/communication"
+      @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Logout'])[1]/following::a[1]").click
+      @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Due Date'])[1]/following::i[1]").click
+      @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Send daily summary'])[2]/following::i[1]").click
+    end
+  end
+
 end
