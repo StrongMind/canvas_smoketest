3bba60c5e485fceff2cf08a6eb2feaf67befd900
Sarah Albinda <sarah.albinda@strongmind.com>
2019-01-24 16:12:13 -0700

    rest of the tests for teacher course settings and navigation

--- spec/teachers/course_settings_spec.rb
+++ spec/teachers/course_settings_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "course settings as a student" do
+describe "course settings as a teacher" do
   before(:all) { configure_driver }
   after(:all)  { @driver.quit }
 
@@ -34,22 +34,43 @@ describe "course settings as a student" do
 
   context 'I can copy a course' do
     it 'I can import a course cartridge' do
-      expect { @driver.find_element(:css, "select#ChooseMigrationConverter") }.to_not raise_error
+      @driver.find_element(:css,"a.import_content").click
+      expect { @driver.find_element(:css, "div.control-group") }.to_not raise_error
     end
 
     it 'I can copy a course via import menu' do
-      @driver.find_element(class: 'import_content').click
-      expect { @driver.find_element(:css, "select#ChooseMigrationConverter") }.to_not raise_error
+      expect { @driver.find_element(:css, "select#chooseMigrationConverter > option:nth-child(2)") }.to_not raise_error
     end
 
     it 'I can export the course' do
-      expect { @driver.find_element(:css, "i.icon-download") }.to_not raise_error
-      expect { @driver.find_element(:css, "div.export_messages") }.to_not raise_error
+      @driver.find_element(:css, "a.settings").click
+      @driver.find_element(:css,"aside#right-side > div > a:nth-child(7)").click
+      expect { @driver.find_element(:css, "form#exporter_form") }.to_not raise_error
+    end
+  end
+
+  context 'I can configure my course navigation and options' do
+    it 'I can choose which navigation items a student will see' do
+      @driver.find_element(:css, "a.settings").click
+      @driver.find_element(:css,"li#navigation_tab").click
+      expect { @driver.find_element(:css, "ul#nav_enabled_list") }.to_not raise_error
+      expect { @driver.find_element(:css, "ul#nav_disabled_list") }.to_not raise_error
     end
 
-    #it 'I can re-import an exported course and the attempt count is maintained' do
-      #expect { @driver.find_element(:css, "div#course_details_tabs") }.to_not raise_error
-    #end
+    it 'I can view edit and add sections to the course' do
+      @driver.find_element(:css, "li#sections_tab").click
+      expect { @driver.find_element(:css,"input#course_section_name") }.to_not raise_error
+    end
 
+    it 'I can edit and add apps tied to the course' do
+      @driver.find_element(:css, "a#tab-tools-link").click
+      expect { @driver.find_element(:css, "span.AddExternalToolButton") }.to_not raise_error
+      expect { @driver.find_element(:css, "div.ExternalToolsTable") }.to_not raise_error
+    end
+
+    it 'I can change feature options for a course' do
+      @driver.find_element(:css, "li#feature_flags_tab").click
+      expect { @driver.find_element(:css, "div.course-feature-flags") }.to_not raise_error
+    end
   end
 end
