0a7921669a1dbbc3de93eda7856048a6622fb89e
Mark Ng <mark@markng.co.uk>
2019-03-11 16:08:29 -0700

    Update course_overview_spec.rb
    Remove byebug comment

--- spec/teachers/course_overview_spec.rb
+++ spec/teachers/course_overview_spec.rb
@@ -8,7 +8,6 @@ describe "course overview as a teacher" do
 
   before(:all) do
     @driver.find_element(class: 'ic-DashboardCard__header-title').click
-    # byebug
   end
 
   context 'I can interact with my modules' do
