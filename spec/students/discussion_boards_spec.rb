8531cf78c491fb7b636a11a4fba38c7ca5e25e13
Chris Irish <irish@velocitylabs.io>
2019-06-13 17:02:12 -0700

    update discussion board spec to be less flaky

--- spec/students/discussion_boards_spec.rb
+++ spec/students/discussion_boards_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe "discussion boards as a student" do
   include_examples "student login"
 
-  before(:all) do
+  # these tests needs more patience
+  before { @driver.manage.timeouts.implicit_wait = 30 }
+  after  { @driver.manage.timeouts.implicit_wait = @config.implicit_wait }
+
+  before(:each) do
+    @driver.get 'https://courseware-staging.strongmind.com/'
+
     @driver.find_element(class: 'ic-DashboardCard__header-title').click
     @driver.find_element(:css, "a.discussions").click
   end
@@ -13,24 +19,22 @@ describe "discussion boards as a student" do
       expect { @driver.find_element(:css, "input#searchTerm") }.to_not raise_error
     end
 
-    it 'I can add a post' do
+    it 'I can add a post, subscribe/unsubscribe, edit, then delete my post' do
       @driver.find_element(:css, "a#new-discussion-btn").click
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[3]/following::button[1]").click
       expect { @driver.find_element(class: 'message') }.to_not raise_error
-    end
 
-    it 'I can subscribe and unsubscribe from my post' do
+      # I can subscribe and unsubscribe from my post
       expect { @driver.find_element(class: 'btn-success') }.to_not raise_error
       expect { @driver.find_element(:css, ".topic-unsubscribe-button") }.to_not raise_error
-    end
 
-    it 'I can edit my post' do
+      # I can edit my post
       @driver.find_element(:css, "a.edit-btn").click
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[3]/following::button[1]").click
-    end
 
-    it 'I can delete my post' do
-      sleep(3)
+      sleep 3
+
+      # I can delete my post
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Press the comma key or shift + question mark to see a list of keyboard shortcuts'])[1]/following::i[3]").click
       @driver.find_element(:id, "ui-id-3").click
       close_alert_and_get_its_text
@@ -41,15 +45,16 @@ describe "discussion boards as a student" do
     end
 
     it 'I can see posts and replies in a hierarchal manner' do
-      sleep(3)
       @driver.find_element(:link, "Discussion: Hymn to the Nile - Hymn to the Nile").click
       expect { @driver.find_element(:css, "ul.discussion-entries") }.to_not raise_error
     end
 
     it 'I can reply to a discussion post' do
+      @driver.find_element(:link, "Discussion: Hymn to the Nile - Hymn to the Nile").click
+      expect { @driver.find_element(:css, "ul.discussion-entries") }.to_not raise_error
+
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Expand replies'])[1]/following::span[1]").click
       @driver.find_element(:xpath, "(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[1]/following::button[1]").click
     end
-
   end
 end
